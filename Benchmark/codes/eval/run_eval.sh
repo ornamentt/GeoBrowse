3640626cec27fd5948dd5fed123c39e66459c551
ornamentt <g1649468858@163.com>
2026-04-05 16:16:19 +0800

    Clear bugs

--- Benchmark/codes/eval/run_eval.sh
+++ Benchmark/codes/eval/run_eval.sh
@@ -1,9 +1,9 @@
-export API_KEY="ms-4fcc0072-2e28-4428-9812-cbc7603ac9e9"
+export API_KEY=""
 export BASE_URL="https://api-inference.modelscope.cn/v1/"
 export JUDGE_MODEL="Qwen/Qwen2.5-72B-Instruct"
 export MAX_WORKERS=8
 
-input_file=/Users/gengxinyu/Documents/codes/GeoBrowse/level1_en.jsonl
+input_file=""
 
 python Benchmark/codes/eval/evaluate.py \
     --input_fp $input_file
